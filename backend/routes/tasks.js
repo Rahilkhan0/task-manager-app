4d155c893a8c577bf06205e9f1ee44b79833a8ec
Rahil Khan <rahil.apsit.5426@gmail.com>
2025-05-19 22:05:44 +0530

    created backend and testes on postman

---
+++ backend/routes/tasks.js
@@ -0,0 +1,76 @@
+ 
+const express = require('express');
+const router = express.Router();
+const Task = require('../models/Task');
+
+//all tasks
+router.get('/', async (req, res) => {
+  try {
+    const tasks = await Task.find().sort({ createdAt: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new task
+router.post('/', async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status || 'Pending'
+  });
+
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Getting a single taskonly
+router.get('/:id', async (req, res) => {
+    try {
+      const task = await Task.findById(req.params.id);
+      if (!task) {
+        return res.status(404).json({ message: 'Task not found' });
+      }
+      res.json(task);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
+// for  updating a task
+router.put('/:id', async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+
+    task.title = req.body.title || task.title;
+    task.description = req.body.description || task.description;
+    task.status = req.body.status || task.status;
+
+    const updatedTask = await task.save();
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// for deleting a task 
+router.delete('/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    }
+    res.json({ message: 'Task deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+module.exports = router;
